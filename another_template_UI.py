f64c6cc9f244aaff380b07f29b372e5a5205936f
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-08-09 22:16:28 +0530

    Renaming_files

--- another_template_UI.py
+++ another_template_UI.py
@@ -17,44 +17,40 @@ canvas = Canvas(height=200, width=200, highlightthickness=0)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 
-canvas.grid(row=0, column=1, pady=10)
+canvas.grid(row=0, column=1, padx=10, pady=10)
 
 # Website label
 website_label = Label(text="Website:", width=16)
 website_label.grid(row=1, column=0, pady=4)
 
 # Website entry
-website_entry = Entry(width=45)
+website_entry = Entry(width=55)
 website_entry.grid(row=1, column=1, columnspan=2, pady=4)
-# To autofocus it as soon as the app is launched.
-website_entry.focus()
 
 # user_name label
 user_name_label = Label(text="Email/Username:", width=16)
 user_name_label.grid(row=2, column=0, pady=4)
 
 # user_name entry
-user_name_entry = Entry(width=45)
+user_name_entry = Entry(width=55)
 user_name_entry.grid(row=2, column=1, columnspan=2, pady=4)
-# Adding predefined email id so that entry is already filled with our primary email id when program starts.
-user_name_entry.insert(0, "dummy@gmail.com")
 
 # pwd label
 pwd_label = Label(text="Password:", width=16)
 pwd_label.grid(row=3, column=0, pady=4)
 
 # pwd entry
-pwd_entry = Entry(width=45)
-pwd_entry.grid(row=3, column=1, columnspan=2, pady=4)
+pwd_entry = Entry(width=26)
+pwd_entry.grid(row=3, column=1, padx=2, pady=4, sticky=W)
 
 # generate password button
-gen_pwd = Button(text="Generate Password", relief="raised", width=17)
-gen_pwd.grid(row=4, column=1, pady=4, sticky="w")
+gen_pwd = Button(text="Generate Password", width=15, relief="raised")
+gen_pwd.grid(row=3, column=2, padx=0, pady=4)
 
 
 # Add button
-add_pwd = Button(text="Add", relief="raised", highlightthickness=0, width=17)
-add_pwd.grid(row=4, column=2, pady=4, sticky="e")
+add_pwd = Button(text="Add", width=47, relief="raised", highlightthickness=0)
+add_pwd.grid(row=4, column=1, columnspan=2, pady=8)
 
 
 window.mainloop()
